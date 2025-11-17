f8b8b217ea6770dfb3f6687af345c454f6ce236b
cmlorbert <cmlorbert@gmail.com>
2025-11-17 01:23:15 -0600

    whoops

--- fcl-fantasy-cycling-app/src/components/TeamPage.jsx
+++ fcl-fantasy-cycling-app/src/components/TeamPage.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import CyclistRow from './cyclistRow';
 
-function TeamPage({userTeam, setRidersAvailable, userTeamName, setUserTeamName}) {
+function TeamPage({userTeam, setUserTeam, setRidersAvailable, userTeamName, setUserTeamName}) {
+  const handleRemoveRider = (riderId) => {
+    const riderToRemove = userTeam.find(r => r.id === riderId);
+    if(!riderToRemove) return;
+
+    setUserTeam(prevTeam => prevTeam.filter(r => r.id !== riderId));
+    setRidersAvailable(prevRiders => [...prevRiders, riderToRemove]);
+  }
 
   return (
     <div>
@@ -17,16 +25,24 @@ function TeamPage({userTeam, setRidersAvailable, userTeamName, setUserTeamName})
                     Cost
                 </th>
                 <th>
-                    Add
+                    Remove
                 </th>
             </tr>
-            {userTeam.map((rider)=> 
-                <tr>
-                    <td>{rider.name}</td>
-                    <td>{rider.team}</td>
-                    <td>{rider.country}</td>
-                    <td>{rider.cost}</td>
-                </tr>)}
+            <tbody>
+              {userTeam.length > 0 ? (
+                userTeam.map(rider => (
+                  <CyclistRow
+                  key={rider.id}
+                  rider={rider}
+                  onAction={handleRemoveRider}
+                  actionType={'remove'}
+                  />
+                ))
+              ) : (
+                <p>Your Team Is Empty</p>
+                  )
+              }
+            </tbody>
         </table>
     </div>
   );
